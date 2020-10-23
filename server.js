caf94cc267c65c6eeea50b9e373ad8aae716d903
Stephen-Roach <stephenroach21@gmail.com>
2020-10-22 20:08:41 -0500

    Completed Step 4

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { use } = require('passport');
+const router = require('./router/main');
+const routerMain = require('./router/main');
 
 const app = express();
 
@@ -6,12 +9,15 @@ app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/', express.static(__dirname + '/public'));
 
+// API Routing
+app.use('/', routerMain);
+
 app.get('/heartbeat', (req, res) => {
   res.json({
     is: 'working',
   });
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Hello humans, server is running');
 });
