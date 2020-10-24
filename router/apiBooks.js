ef394d18c96aeb90be30d57d3f2806b9f89d3b8d
Stephen-Roach <stephenroach21@gmail.com>
2020-10-24 11:00:23 -0500

    finished step 4b and 5

---
+++ router/apiBooks.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+
+router.get('/', async (req, res) => {
+  const { author, title } = req.body;
+  const params = `${title ? 'title=' + title : ''}${
+    author ? '&author=' + author : ''
+  }`;
+  fetch(`http://openlibrary.org/search.json?${params}&limit=10`)
+    .then((result) => result.json())
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
 const { use } = require('passport');
 const router = require('./router/main');
-const routerMain = require('./router/main');
-
 const app = express();
+const fetch = require('node-fetch');
+const bodyParer = require('body-parser');
+
+const apiHeartBeat = require('./router/apiHeartbeat');
+const booksRouter = require('./router/apiBooks');
+const routerMain = require('./router/main');
 
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/', express.static(__dirname + '/public'));
+app.use(bodyParer.json());
 
 // API Routing
 app.use('/', routerMain);
-
-app.get('/heartbeat', (req, res) => {
-  res.json({
-    is: 'working',
-  });
-});
+app.use('/router/apiHeartBeat', apiHeartBeat);
+app.use('/router/apiBooks', booksRouter);
 
 app.listen(3001, () => {
   console.log('Hello humans, server is running');
